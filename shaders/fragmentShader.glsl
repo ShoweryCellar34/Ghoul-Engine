1d949762e3ced1ddf67be68e101c979454459891
ShoweryCellar34 <ShoweryCellar34@users.noreply.github.com>
2023-10-25 15:11:45 +1300

    modified:   shaders/fragmentShader.glsl

--- shaders/fragmentShader.glsl
+++ shaders/fragmentShader.glsl
@@ -1,4 +1,4 @@
-#version 330 core
+#version 460 core
 
 out vec4 FragColor;
 
@@ -10,5 +10,5 @@ uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2) * ourColor;
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.5) * ourColor;
 }
