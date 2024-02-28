19128d234f3f1dd59b7f28c84024b0d120016246
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-02-28 15:02:23 +1300

    modified:   Pentagram 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-#include <Pentagram.hpp>
+//#include <Pentagram.hpp>
+//
+//int main(int argc, char *argv[])
+//{
+//    return 0;
+//}
+//
+#include <ctime>
+#include <iostream>
 
-int main(int argc, char *argv[])
-{
-    return 0;
+int main() {
+    std::time_t t = std::time(0);   // get time now
+    std::tm* now = std::localtime(&t);
+    std::cout << (now->tm_year + 1900) << '-' 
+         <<  now->tm_mday << '-'
+         << (now->tm_mon + 1)
+         << "\n";
 }
