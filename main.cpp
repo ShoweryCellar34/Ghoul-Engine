dece830d21171a8c39228dc00dd4ca73c4a74490
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-03-07 15:10:14 +1300

    modified:   .vscode/settings.json 	modified:   Pentagram 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,8 @@
-//#include <Pentagram.hpp>
-//
-//int main(int argc, char *argv[])
-//{
-//    return 0;
-//}
-//
-// C++ Program to implement Date and Time parsing using
-// <ctime>
-// C++ Program to implement Date and Time Parsing using
-// chrono
-#include <iostream>
-#include <ctime>
+#include <Pentagram.hpp>
 
-int main() {
-    // Get the current time
-    time_t currentTime = time(0);
-
-    // Create a buffer to store the formatted time
-    char buffer[80];
-
-    // Format the current time
-    strftime(buffer, 80, "%d-%m-%Y %H:%M:%S", localtime(&currentTime));
-
-    std::cout << "The current time in the specified format is: " << buffer << std::endl;
-
+int main(int argc, char *argv[])
+{
+    logger log;
+    log.log(1, "Hello");
     return 0;
 }
