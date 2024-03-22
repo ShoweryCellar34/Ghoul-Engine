c2306f373aba35ba4201bbecc713bc7166ea8603
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-03-22 14:47:48 +1300

    modified:   Pentagram 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,12 @@ int main(int argc, char *argv[])
 {
     PNT::initialize(800, 500, "Ghoul Engine");
     PNT::vsync(PNT::ON);
+    bool running = true;
+    while(running)
+    {
+        SDL_Event event = PNT::startFrame(&running);
+        ImGui::ShowDemoWindow();
+        PNT::endFrame();
+    }
     return 0;
 }
