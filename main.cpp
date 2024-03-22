3a6c798da7f84b80137bd03f0dfb6e81dc36457f
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-03-22 14:56:50 +1300

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -7,14 +7,7 @@ int main(int argc, char *argv[])
     bool running = true;
     while(running)
     {
-        SDL_Event event = PNT::startFrame(&running);
-        while (SDL_PollEvent(&event))
-        {
-            if(event.type = SDL_EVENT_QUIT)
-            {
-                running = false;
-            }
-        }
+        PNT::startFrame(&running);
         ImGui::ShowDemoWindow();
         PNT::endFrame();
     }
