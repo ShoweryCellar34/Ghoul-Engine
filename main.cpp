3c9f468540bcf3fc466aaec74a5862860f6e6751
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-03-28 13:36:37 +1300

    modified:   Pentagram 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,25 @@ int main(int argc, char *argv[])
     while(running)
     {
         static unsigned short rgba[8];
+        static bool rgbaRondomize = false;
         SDL_Event event = PNT::startFrame(&running, rgba[0], rgba[2], rgba[4], rgba[6]);
-        ImGui::Begin("Background Color");
-        ImGui::Text("RGBA: ");
+        ImGui::Begin("Rehehehehehehehe");
+
+        ImGui::Text("Backgroung RGBA: ");
         ImGui::SameLine();
         ImGui::SliderInt4("##SliderInt4 0", (int *)rgba, 0, 255, "%d", ImGuiSliderFlags_AlwaysClamp);
+
+        ImGui::Text("Backgroung RGBA Randomize (flashing lights): ");
+        ImGui::SameLine();
+        ImGui::Checkbox("##Checkbox 0", &rgbaRondomize);
+        if(rgbaRondomize)
+        {
+            rgba[0] = rand() % 255 + 1;
+            rgba[2] = rand() % 255 + 1;
+            rgba[4] = rand() % 255 + 1;
+            rgba[6] = rand() % 255 + 1;
+        }
+
         ImGui::End();
         PNT::endFrame();
     }
