26c152177ca20085410847d2dee979b012d388e1
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-04-04 13:20:22 +1300

    modified:   Pentagram 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -7,33 +7,37 @@ int main(int argc, char *argv[])
     PNT::initialize(800, 500, "Ghoul Engine");
     PNT::vsync(PNT_VSYNC_ON);
     PNT::Window test("Hello", 500, 500, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
-    PNT::Window test2("Hello", 500, 500);
+    PNT::Window test2("Hello", 500, 500, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
     bool running = true;
     while(running)
     {
+        test.startFrame();
         test.eventProcess();
+        test.endFrame();
+        test2.startFrame();
         test2.eventProcess();
+        test2.endFrame();
         static unsigned short rgba[8];
-        static bool rgbaRondomize = false;
+        //static bool rgbaRondomize = false;
         SDL_Event event = PNT::startFrame(&running, rgba[0], rgba[2], rgba[4], rgba[6]);
-        ImGui::Begin("Rehehehehehehehe");
+        //ImGui::Begin("Rehehehehehehehe");
 
-        ImGui::Text("Backgroung RGBA: ");
-        ImGui::SameLine();
-        ImGui::SliderInt4("##SliderInt4 0", (int *)rgba, 0, 255, "%d", ImGuiSliderFlags_AlwaysClamp);
+        //ImGui::Text("Backgroung RGBA: ");
+        //ImGui::SameLine();
+        //ImGui::SliderInt4("##SliderInt4 0", (int *)rgba, 0, 255, "%d", ImGuiSliderFlags_AlwaysClamp);
 
-        ImGui::Text("Backgroung RGBA Randomize (flashing lights): ");
-        ImGui::SameLine();
-        ImGui::Checkbox("##Checkbox 0", &rgbaRondomize);
-        if(rgbaRondomize)
-        {
-            rgba[0] = rand() % 255 + 1;
-            rgba[2] = rand() % 255 + 1;
-            rgba[4] = rand() % 255 + 1;
-            rgba[6] = rand() % 255 + 1;
-        }
+        //ImGui::Text("Backgroung RGBA Randomize (flashing lights): ");
+        //ImGui::SameLine();
+        //ImGui::Checkbox("##Checkbox 0", &rgbaRondomize);
+        //if(rgbaRondomize)
+        //{
+            //rgba[0] = rand() % 255 + 1;
+            //rgba[2] = rand() % 255 + 1;
+            //rgba[4] = rand() % 255 + 1;
+            //rgba[6] = rand() % 255 + 1;
+        //}
 
-        ImGui::End();
+        //ImGui::End();
         PNT::endFrame();
     }
     PNT::deinitialize();
