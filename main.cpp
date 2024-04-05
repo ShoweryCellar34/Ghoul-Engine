dcbc322eb42a38478cf8b8ffd0eebaf4fa21588b
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-04-05 14:57:35 +1300

    modified:   Pentagram 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -6,20 +6,24 @@ int main(int argc, char *argv[])
 {
     PNT::initialize(800, 500, "Ghoul Engine");
     PNT::vsync(PNT_VSYNC_ON);
-    PNT::Window test("test", 500, 500, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
-    PNT::Window test2("test2", 500, 500, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
+    PNT::Window test[2] = {PNT::Window(), PNT::Window()};
     bool running = true;
     while(running)
     {
-        test.startFrame(127, 127, 127);
-        test.eventProcess();
-        test.endFrame();
-        test2.startFrame(100, 50, 200);
-        test2.eventProcess();
-        test2.endFrame();
+        for(PNT::Window window : test)
+        {
+            window.startFrame();
+            SDL_Event event;
+            while(SDL_PollEvent(&event))
+            {
+                
+            }
+            window.endFrame();
+        }
+
         static unsigned short rgba[8];
         static bool rgbaRondomize = false;
-        SDL_Event event = PNT::startFrame(&running, rgba[0], rgba[2], rgba[4], rgba[6]);
+        PNT::startFrame(&running, rgba[0], rgba[2], rgba[4], rgba[6]);
         ImGui::Begin("Rehehehehehehehe");
 
         ImGui::Text("Backgroung RGBA: ");
