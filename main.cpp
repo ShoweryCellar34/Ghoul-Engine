2b7cb38e27eb111177b00bcfb147ef9d66547fb3
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-04-14 08:15:22 +1200

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -6,47 +6,50 @@ unsigned short step = 1;
 
 void testCallback(PNT::Window *window, SDL_Event event)
 {
-    switch(event.key.keysym.sym)
+    if(event.type == SDL_EVENT_KEY_DOWN)
     {
-    case SDLK_1:
-        rgb[0] = 1;
-        rgb[1] = 0;
-        rgb[2] = 0;
-        break;
+        switch(event.key.keysym.sym)
+        {
+        case SDLK_1:
+            rgb[0] = 1;
+            rgb[1] = 0;
+            rgb[2] = 0;
+            break;
 
-    case SDLK_2:
-        rgb[0] = 0;
-        rgb[1] = 0;
-        rgb[2] = 1;
-        break;
+        case SDLK_2:
+            rgb[0] = 0;
+            rgb[1] = 0;
+            rgb[2] = 1;
+            break;
 
-    case SDLK_BACKSPACE:
-        window->setPosition(SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
-        break;
+        case SDLK_BACKSPACE:
+            window->setPosition(SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+            break;
 
-    case SDLK_EQUALS:
-        if(event.type == SDL_EVENT_KEY_DOWN) step++;
-        break;
+        case SDLK_EQUALS:
+            step++;
+            break;
 
-    case SDLK_MINUS:
-        if(event.type == SDL_EVENT_KEY_DOWN) step--;
-        break;
+        case SDLK_MINUS:
+            step--;
+            break;
 
-    case SDLK_UP:
-        window->setPosition(-1, window->getPosition().second - step);
-        break;
+        case SDLK_UP:
+            window->setPosition(-1, window->getPosition().second - step);
+            break;
 
-    case SDLK_DOWN:
-        window->setPosition(-1, window->getPosition().second + step);
-        break;
+        case SDLK_DOWN:
+            window->setPosition(-1, window->getPosition().second + step);
+            break;
 
-    case SDLK_LEFT:
-        window->setPosition(window->getPosition().first - step, -1);
-        break;
+        case SDLK_LEFT:
+            window->setPosition(window->getPosition().first - step, -1);
+            break;
 
-    case SDLK_RIGHT:
-        window->setPosition(window->getPosition().first + step, -1);
-        break;
+        case SDLK_RIGHT:
+            window->setPosition(window->getPosition().first + step, -1);
+            break;
+        }
     }
 }
 
