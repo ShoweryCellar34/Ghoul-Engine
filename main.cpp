a76a3ebb33ac2111d3be0b6547c1c5d4d40c983c
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-05-02 11:42:34 +1200

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <PentagramExt.hpp>
 #include <random>
 
-bool shouldClose = false;
 PNT::Window window("Demo Window", 600, 600, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
 float rgb[] = {1, 0, 0};
 unsigned char step = 5;
@@ -51,41 +50,41 @@ void eventListener()
         case SDLK_RIGHT:
             window.setPosition(window.getWindowData().x + step, -1);
             break;
-
-        case SDL_EVENT_QUIT:
-            shouldClose = true;
-            break;
         }
     }
-}
-
-void startFrameListener()
-{
-    // Begin frame
-    window.setClearColor(rgb[0], rgb[1], rgb[2]);
-    window.startFrame();
-
-    // ImGui
-    ImGui::Begin("Demo Controls");
-
-    ImGui::Text("Background Color: ");
-    ImGui::ColorPicker3("##ColorPicker3 0", rgb, ImGuiColorEditFlags_InputRGB);
-
-    ImGui::Text("Reposition Step: ");
-    ImGui::SameLine();
-    ImGui::SliderInt("##SliderInt 0", (int *)&step, 1, 20, "%d", ImGuiSliderFlags_AlwaysClamp);
-
-    ImGui::End();
 }
 
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
     window.setListener(PNT_LISTENER_FLAGS_EVENT, &eventListener);
-    window.setListener(PNT_LISTENER_FLAGS_STARTFRAME, &startFrameListener);
+    bool shouldClose = false;
     while(!shouldClose)
     {
-        PNT::eventProcess();
+        // Event processing
+        while(SDL_PollEvent(&PNT::Window::event))
+        {
+            window.eventProcess(&shouldClose);
+        }
+
+        // Begin frame
+        window.setClearColor(rgb[0], rgb[1], rgb[2]);
+        window.startFrame();
+
+        // ImGui
+        ImGui::Begin("Demo Controls");
+
+        ImGui::Text("Background Color: ");
+        ImGui::ColorPicker3("##ColorPicker3 0", rgb, ImGuiColorEditFlags_InputRGB);
+
+        ImGui::Text("Reposition Step: ");
+        ImGui::SameLine();
+        ImGui::SliderInt("##SliderInt 0", (int *)&step, 1, 20, "%d", ImGuiSliderFlags_AlwaysClamp);
+
+        ImGui::End();
+
+        // End frame
+        window.endFrame();
     }
     return 0;
 }
