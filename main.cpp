69a8e9a7ba56077f38a6b00aa27f49f64e9a4174
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-05-14 08:42:13 +1200

    modified:   Pentagram 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@ bool shouldClose = false;
 void eventListener(PNT::Window *window);
 void startFrameListener(PNT::Window *window);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     // Initualizing SDL
     SDL_Init(SDL_INIT_VIDEO);
 
@@ -13,12 +12,12 @@ int main(int argc, char *argv[])
     PNT::Window window("Ghoul Engine", 1000, 800, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
 
     // Setting listeners
-    window.setListener(PNT_LISTENER_FLAGS_EVENT, &eventListener);
+    window.setListener(PNT_LISTENER_FLAGS_KEYBOARDEVENT, &eventListener);
+    window.setListener(PNT_LISTENER_FLAGS_MOUSEEVENT, &eventListener);
     window.setListener(PNT_LISTENER_FLAGS_STARTFRAME, &startFrameListener);
 
     // App loop
-    while(!shouldClose)
-    {
+    while(!shouldClose) {
         // Event processing
         while(PNT::pollEvent())
         {
@@ -32,10 +31,8 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void eventListener(PNT::Window *window)
-{
-    switch(PNT::getEvent().key.keysym.sym)
-    {
+void eventListener(PNT::Window *window) {
+    switch(PNT::getEvent().key.keysym.sym) {
         case SDLK_1:
             window->setClearColor(1.0f, 0.0f, 0.0f);
             break;
@@ -48,13 +45,9 @@ void eventListener(PNT::Window *window)
             window->setClearColor(0.0f, 0.0f, 0.0f);
             break;
     }
-    if(PNT::getEvent().window.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED)
-    {
-        shouldClose = true;
-    }
+    if(PNT::getEvent().window.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED) shouldClose = true;
 }
 
-void startFrameListener(PNT::Window *window)
-{
+void startFrameListener(PNT::Window *window) {
     ImGui::ShowDemoWindow();
 }
