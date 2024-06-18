4b7c68aecef454b97901557cea488447c8de8bb2
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-06-18 13:28:51 +1200

    modified:   CmakeLists.txt 	modified:   Pentagram 	deleted:    ghoul.png 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,28 +3,25 @@
 void eventCallback(PNT::Window *window, PNT::windowEvent event) {
 }
 
-void startFrameCallback(PNT::Window *window) {
-    ImGui::Begin("Controls");
-    ImGui::End();
-}
-
 int main(int argc, char *argv[]) {
     PNT::init();
 
     PNT::Window window("Ghoul Engine", 1000, 700, 200, 200, ImGuiConfigFlags_ViewportsEnable | ImGuiConfigFlags_DockingEnable);
-    window.setCallback(PNT_CALLBACK_FLAGS_STARTFRAME, &startFrameCallback);
     window.setEventCallback(&eventCallback);
     window.setClearColor(0.25f, 0.25f, 0.25f, 0.25f);
-    PNT::image icon("..\\ghoul.png", 4);
-    icon.resize(32, 32);
+
+    // Set icon.
+    PNT::image icon("res\\ghoul32x32.png", 4);
     window.setIcon(icon);
-    icon.loadOnGPU();
 
     while(!window.shouldClose()) {
         PNT::processEvents();
 
         window.startFrame();
-        icon.ImGuiDraw(512, 512);
+
+        ImGui::Begin("Controls");
+        ImGui::End();
+
         window.endFrame();
     }
 
