2305ed81ccebf95b6afdfa0e383837ea35e9108f
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-06-22 11:33:57 +1200

    modified:   Pentagram 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Pentagram.hpp>
 
-void eventCallback(PNT::Window *window, PNT::windowEvent event) {
+void eventCallback(PNT::Window* window, PNT::windowEvent event) {
 }
 
 int main(int argc, char *argv[]) {
@@ -8,53 +8,9 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    PNT::Window window("Ghoul Engine", 1000, 700, 200, 200, ImGuiConfigFlags_ViewportsEnable | ImGuiConfigFlags_DockingEnable);
+    PNT::Window window("Drag'n Drop");
     window.setEventCallback(eventCallback);
-    window.setClearColor(0.25f, 0.25f, 0.25f, 0.25f);
+    PNT::file imageFile("res\\textures\\logo\\ghoul.png");
+    PNT::image image(imageFile)
 
-    // Set icon.
-    PNT::image icon("res\\textures\\logo\\ghoul32x32.png");
-    window.setIcon(icon);
-
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    };
-
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Vertex shader.
-    PNT::file vertexFile("res\\shaders\\vertex.glsl");
-    PNT::shader vertexShader(vertexFile.getContents(), GL_VERTEX_SHADER);
-    vertexShader.compile();
-
-    // Fragment shader.
-    PNT::file fragmentFile("res\\shaders\\fragment.glsl");
-    PNT::shader fragmentShader(fragmentFile.getContents(), GL_FRAGMENT_SHADER);
-    fragmentShader.compile();
-
-    // Shader program.
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader.getID());
-    glAttachShader(shaderProgram, fragmentShader.getID());
-    glLinkProgram(shaderProgram);
-
-    while(!window.shouldClose()) {
-        PNT::processEvents();
-
-        window.startFrame();
-
-        ImGui::Begin("Controls");
-        ImGui::End();
-
-        window.endFrame();
-    }
-
-    PNT::deinit();
-    return 0;
 }
