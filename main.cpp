d7652b19014df04640a6492895e82f8631316243
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-06-21 20:32:01 +1200

    modified:   Pentagram 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -26,27 +26,19 @@ int main(int argc, char *argv[]) {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // Vertex shader.
-    char* shaderSource = (char*)PNT::fileToString("res\\shaders\\vertex.glsl").c_str();
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &shaderSource, NULL);
-    glCompileShader(vertexShader);
+    PNT::shader vertexShader(PNT::fileToString("res\\shaders\\vertex.glsl").c_str(), GL_VERTEX_SHADER);
+    vertexShader.compile();
 
     // Fragment shader.
-    shaderSource = (char*)PNT::fileToString("res\\shaders\\fragment.glsl").c_str();
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &shaderSource, NULL);
-    glCompileShader(fragmentShader);
+    PNT::shader fragmentShader(PNT::fileToString("res\\shaders\\fragment.glsl").c_str(), GL_FRAGMENT_SHADER);
+    fragmentShader.compile();
 
     // Shader program.
     unsigned int shaderProgram;
     shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
+    glAttachShader(shaderProgram, vertexShader.getID());
+    glAttachShader(shaderProgram, fragmentShader.getID());
     glLinkProgram(shaderProgram);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
 
     while(!window.shouldClose()) {
         PNT::processEvents();
