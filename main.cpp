1e4016c5f00d642d1e379030dfe9f04f7c6cf4c9
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-06-25 14:04:06 +1200

    modified:   Pentagram 	modified:   main.cpp 	modified:   res/shaders/fragment.glsl 	modified:   res/shaders/vertex.glsl

--- main.cpp
+++ main.cpp
@@ -16,37 +16,70 @@ int main(int argc, char *argv[]) {
     PNT::image icon("res\\textures\\logo\\ghoul32x32.png");
     window.setIcon(icon);
 
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    };
+    PNT::file shaderSource("res\\shaders\\vertex.glsl");
 
-    unsigned int VBO;
+    PNT::shader vertexShader(shaderSource.getContents(), GL_VERTEX_SHADER);
+    vertexShader.compile();
+    std::cout << vertexShader.getError() << "\n\n\n";
+
+    // fragment shader
+    shaderSource.open("res\\shaders\\fragment.glsl");
+    PNT::shader fragmentShader(shaderSource.getContents(), GL_FRAGMENT_SHADER);
+    fragmentShader.compile();
+    std::cout << fragmentShader.getError() << "\n\n\n";
+
+    // link shaders
+    PNT::program shaderProgram({&vertexShader, &fragmentShader});
+    shaderProgram.link();
+    std::cout << shaderProgram.getError() << "\n\n\n";
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Vertex shader.
-    PNT::file file("res\\shaders\\vertex.glsl");
-    PNT::shader vertexShader(file.getContents(), GL_VERTEX_SHADER);
-    vertexShader.compile();
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // Fragment shader.
-    file.open("res\\shaders\\fragment.glsl");
-    PNT::shader fragmentShader(file.getContents(), GL_FRAGMENT_SHADER);
-    fragmentShader.compile();
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-    // Shader program.
-    PNT::program shaderProgram({&vertexShader, &fragmentShader});
-    shaderProgram.link();
-    printf("%s\n", shaderProgram.getError());
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
 
     while(!window.shouldClose()) {
         PNT::processEvents();
 
         window.startFrame();
 
+        shaderProgram.use();
+        glBindVertexArray(VAO);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         ImGui::Begin("Controls");
         ImGui::End();
 
--- res/shaders/vertex.glsl
+++ res/shaders/vertex.glsl
@@ -1,7 +1,6 @@
-#version 460 core
-layout (location = 0) in vec3 aPos;
-
+#version 330 core
+out vec4 FragColor;
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
 }
