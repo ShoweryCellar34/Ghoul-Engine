b448349a2009fb5f7c8a2b1db74de41d0770a669
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-07-31 15:16:54 +1200

    Fixes

--- main.cpp
+++ main.cpp
@@ -1,87 +1,26 @@
-#include <iostream>
 #include <Pentagram.hpp>
+#include <depracated/file.hpp>
 
-void eventCallback(PNT::Window *window, PNT::windowEvent event) {
+void eventCallback(PNT::Window* window, PNT::windowEvent event) {
+    switch(event.type) {
+    }
 }
 
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-
 int main(int argc, char *argv[]) {
+    stbi_set_flip_vertically_on_load(true);
+
     if(!PNT::init()) {
-        return 1;
+        exit(EXIT_FAILURE);
     }
 
-    PNT::Window window("Ghoul Engine", 1000, 700, 200, 200, ImGuiConfigFlags_ViewportsEnable | ImGuiConfigFlags_DockingEnable);
+    PNT::Window window("Ghoul Engine", 1200, 675, 250, 250, ImGuiConfigFlags_DockingEnable | ImGuiConfigFlags_ViewportsEnable);
+    window.setAspectRatio(16, 9);
     window.setEventCallback(eventCallback);
-    window.setClearColor(0.25f, 0.25f, 0.25f, 0.25f);
-
-    // Set icon.
-    PNT::image icon("res\\textures\\logo\\ghoul32x32.png");
-    window.setIcon(icon);
-    PNT::file a("res\\shaders\\fragment.glsl");
-    std::cout << a.getContents();
-
-    PNT::shader vertexShader(a.getContents().c_str(), GL_VERTEX_SHADER);
-    vertexShader.compile();
-
-    // fragment shader
-    PNT::shader fragmentShader(fragmentShaderSource, GL_FRAGMENT_SHADER);
-    fragmentShader.compile();
-
-    // link shaders
-    PNT::program shaderProgram({&vertexShader, &fragmentShader});
-    shaderProgram.link();
-
-    float vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
-    };
-
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
 
     while(!window.shouldClose()) {
         PNT::processEvents();
 
         window.startFrame();
-
-        shaderProgram.use();
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-
-        ImGui::Begin("Controls");
-        ImGui::End();
 
         window.endFrame();
     }
