b5bc4d4c5886d45fe40d531bc961ca7c7a491376
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-08-13 19:06:16 +1200

    rewriting

--- headers/node.hpp
+++ headers/node.hpp
@@ -1,75 +1,31 @@
 #pragma once
 
-#include <unordered_map>
+#include <string>
 #include <vector>
-#include <scene.hpp>
 
-// namespace PNT {
-//     class Window;
-// }
+typedef size_t nodeID;
 
-// struct node {
-// private:
-//     scene* parentScene;
-//     size_t ID;
-//     char* name;
-//     std::vector<node*> children;
-//     mutable int selectedFlag;
-//     char* imguiName;
-//     node* parent;
-//     mutable bool shouldOpen;
-
-//     friend void drawNodeInspector(const PNT::Window& window);
-//     friend void drawPopup(node* node);
-// public:
-//     node(scene* scene, size_t nodeParent, size_t nodeID, const char* name);
-//     node(scene* scene, size_t nodeParent, size_t nodeID);
-//     ~node();
-
-//     node* addChild();
-//     node* addChild(const char* name);
-//     bool deleteChild(const char* name);
-//     bool deleteChild(size_t ID);
-//     void setName(const char* name);
-//     node* reparent(size_t ID);
-
-//     std::vector<node*> getChildren() const;
-//     node* getChild(const char* name) const;
-//     node* getChild(size_t ID) const;
-//     const char* getName() const;
-//     size_t getID() const;
-//     node* getParent() const;
-
-//     void ImGuiDraw() const;
-// };
-
-struct node : scene {
+struct node {
 private:
-    scene* m_ParentScene;
-    nodeID m_Parent;
+    std::string m_name;
     nodeID m_ID;
-    char* m_Name;
-    char* m_ImguiName;
-    mutable int m_SelectedFlag;
-    mutable bool m_ShouldOpen;
+    std::vector<node> m_children;
+    node* m_parent;
 
-    friend void drawPopup(node* node);
 public:
-    nodeID newNode(nodeID parent, const char* name) = delete;
-    void deleteNode(nodeID ID) = delete;
-    void selectID(nodeID ID) = delete;
-    nodeID getSelectedID() const = delete;
-    node* getNode(nodeID ID) const = delete;
-
-    node(scene* scene, nodeID parent, nodeID ID, const char* name);
+    node(const char* name);
     ~node();
 
     void setName(const char* name);
+    void setName(std::string name);
+    void addChild(const char* name);
+    void addChild(std::string name);
+    void addChild();
     void reparent(nodeID newParent);
-    nodeID addChild(const char* name);
 
+    const char* getName() const;
+    nodeID getChild(const char* name) const;
+    nodeID getChild(std::string name) const;
     nodeID getParent() const;
     nodeID getID() const;
-
-    void ImDraw() const;
 };
--- headers/scene.hpp
+++ headers/scene.hpp
@@ -1,39 +1,6 @@
 #pragma once
 
-#include <unordered_map>
-#include <vector>
-
-struct node;
-
-typedef size_t nodeID;
+#include <node.hpp>
 
 struct scene {
-private:
-    char* name;
-    std::unordered_map<nodeID, node*> nodes;
-    std::vector<nodeID> children;
-    nodeID selectedNode;
-    nodeID nodeIDCounter;
-
-    friend struct node;
-public:
-    scene(const char* name);
-    scene();
-    ~scene();
-
-    nodeID newNode(nodeID parent, const char* name);
-    void deleteNode(nodeID ID);
-    void selectID(nodeID ID);
-
-    nodeID getSelectedID() const;
-    node* getNode(nodeID ID) const;
-
-    void setName(const char* title);
-    nodeID addChild(const char* name);
-    bool removeChild(nodeID ID);
-
-    const char* getName() const;
-    std::vector<nodeID> getChildren() const;
-
-    void ImDraw() const;
 };
