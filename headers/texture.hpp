f4630542425d59190e9faf60b459ec374a8b98c8
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2024-10-17 12:36:56 +1300

    fleshing out texture API

--- headers/texture.hpp
+++ headers/texture.hpp
@@ -1,17 +1,31 @@
 #pragma once
 
-#include <cstdint>
+#include <cstddef>
+#include <string>
+#include <glad/gl.h>
 
 namespace GH {
     class texture {
     private:
+        GladGLContext* m_GL;
         size_t m_ID, m_width, m_height;
         unsigned char* m_data;
 
     public:
         texture();
-        texture(unsigned char* data, size_t width, size_t height);
+        texture(GladGLContext* GL, unsigned char* data, size_t width, size_t height);
+        ~texture();
+
         texture(const texture&) = delete;
         texture& operator=(const texture&) = delete;
+
+        void load();
+        void unload();
+        void setData(const std::string& data);
+        void setGL(GladGLContext* GL);
+
+        const unsigned char* getData();
+        size_t getID();
+        const GladGLContext* getGL();
     };
 }
